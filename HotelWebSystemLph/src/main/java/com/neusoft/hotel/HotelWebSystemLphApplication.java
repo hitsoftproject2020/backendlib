78fb76bceb8e03cce1ab6150bd35673b1cc6bed6
LuoPenghao <2357861275@qq.com>
2020-07-28 11:05:34 +0800

    罗鹏豪第一次模板提交

---
+++ HotelWebSystemLph/src/main/java/com/neusoft/hotel/HotelWebSystemLphApplication.java
@@ -0,0 +1,13 @@
+package com.neusoft.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelWebSystemLphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelWebSystemLphApplication.class, args);
+	}
+
+}
