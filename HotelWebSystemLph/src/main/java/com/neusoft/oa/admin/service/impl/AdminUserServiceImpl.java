5c50a1c97c450ae04b5ad935988afa9c44044593
LuoPenghao <2357861275@qq.com>
2020-07-29 20:21:43 +0800

    debug-罗鹏豪

--- HotelWebSystemLph/src/main/java/com/neusoft/oa/admin/service/impl/AdminUserServiceImpl.java
+++ HotelWebSystemLph/src/main/java/com/neusoft/oa/admin/service/impl/AdminUserServiceImpl.java
@@ -11,41 +11,41 @@ import com.neusoft.oa.admin.model.AdminUserModel;
 import com.neusoft.oa.admin.service.IAdminUserService;
 @Service
 @Transactional  //环绕事务Advice的切入点
-public class AdminUserServiceimpl implements IAdminUserService {
+public class AdminUserServiceImpl implements IAdminUserService {
 	@Autowired
-	private IAdminUserMapper admin = null;
+	private IAdminUserMapper admin11 = null;
 
 	@Override
 	public void insert(AdminUserModel adminUserModel) throws Exception {
 		// TODO Auto-generated method stub
-		admin.insert(adminUserModel);
+		admin11.insert(adminUserModel);
 
 	}
 
 	@Override
 	public void delete(AdminUserModel adminUserModel) throws Exception {
 		// TODO Auto-generated method stub
-		admin.delete(adminUserModel);
+		admin11.delete(adminUserModel);
 
 	}
 
 	@Override
 	public void update(AdminUserModel adminUserModel) throws Exception {
 		// TODO Auto-generated method stub
-		admin.update(adminUserModel);
+		admin11.update(adminUserModel);
 
 	}
 
 	@Override
 	public List<AdminUserModel> selectByAll() throws Exception {
 		// TODO Auto-generated method stub
-		return admin.selectByAll();
+		return admin11.selectByAll();
 	}
 
 	@Override
 	public AdminUserModel selectById(int id) throws Exception {
 		// TODO Auto-generated method stub
-		return admin.selectByid(id);
+		return admin11.selectByid(id);
 	}
 
 }
