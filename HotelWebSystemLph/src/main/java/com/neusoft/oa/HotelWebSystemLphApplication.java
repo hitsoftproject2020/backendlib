59c33a7e70e6423205c47a0d658fcd9f1bf034de
LuoPenghao <2357861275@qq.com>
2020-07-29 08:51:50 +0800

    罗鹏豪service修改

---
+++ HotelWebSystemLph/src/main/java/com/neusoft/oa/HotelWebSystemLphApplication.java
@@ -0,0 +1,20 @@
+package com.neusoft.oa;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@SpringBootApplication
+@MapperScan(basePackages={"com.neusoft.oa.ht.mapper","com.neusoft.oa.admin.mapper"})
+@EnableTransactionManagement  //启用事务Advice
+public class HotelWebSystemLphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelWebSystemLphApplication.class, args);
+		
+		
+	}
+
+}
--- HotelWebSystemLph/src/main/java/com/neusoft/oa/ht/service/impl/CustomerServiceImpl.java
+++ HotelWebSystemLph/src/main/java/com/neusoft/oa/ht/service/impl/CustomerServiceImpl.java
@@ -2,19 +2,28 @@ package com.neusoft.oa.ht.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.neusoft.oa.ht.mapper.ICustomerMapper;
 import com.neusoft.oa.ht.model.CustomerModel;
 import com.neusoft.oa.ht.service.ICustomerService;
-@Service
+@Service("customerService")
 @Transactional  //环绕事务Advice的切入点
 public class CustomerServiceImpl implements ICustomerService {
-
+	
+	@Autowired
+	private ICustomerMapper iCustomerMapper = null;
+	
+	public void setDepartmentMapper(ICustomerMapper iCustomerMapper1) {
+		this.iCustomerMapper = iCustomerMapper1;
+	}
+	
 	@Override
 	public void add(CustomerModel customerModel) throws Exception {
 		// TODO Auto-generated method stub
-		
+		this.iCustomerMapper.insert(customerModel);
 
 	}
 
